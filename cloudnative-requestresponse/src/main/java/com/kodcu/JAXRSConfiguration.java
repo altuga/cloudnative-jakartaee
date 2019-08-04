ae72734d5a81337ee405aeecd106b1c6ff952649
AltugAltintas <altuga@gmail.com>
2019-08-04 11:49:08 +0300

    first commit with cloudnative-requestresponse

---
+++ cloudnative-requestresponse/src/main/java/com/kodcu/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package com.kodcu;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author Kodcu.com
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
