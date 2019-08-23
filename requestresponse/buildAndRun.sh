8df1d741b6d8502718b98a0807317cdd9c9a27fb
AltugAltintas <altuga@gmail.com>
2019-08-23 22:43:44 +0300

    add needed files

---
+++ requestresponse/buildAndRun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mvn clean package && docker build -t com.airhacks/requestresponse .
+docker rm -f requestresponse || true && docker run -d -p 8080:8080 -p 4848:4848 --name requestresponse com.airhacks/requestresponse 
